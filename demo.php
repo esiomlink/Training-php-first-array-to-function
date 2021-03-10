4ea3ae2a310ac861a0ce344109ef3dd475c6e14f
ecoffet simon <simon.ecoffet@gmail.com>
2021-03-10 21:51:58 +0100

    ajout condition commentaire

--- demo.php
+++ demo.php
@@ -7,9 +7,11 @@ $eleve = [
     'age' => 30 ,
     'note' => [15, 20, 16]
 ];
-
 $long=count($eleve['note']);
 $total =0;
+$commentaire;
+
+
 function moyene(){
     global $eleve;
     global $long;
@@ -19,10 +21,18 @@ function moyene(){
         $total=$total+$eleve['note'][$i];
     }
     $total=$total/$long;
+
 };
 moyene($eleve['note']);
 
+if ($total > 15){
+    $commentaire="c'est très bien";
+}else if($total < 15 && $total > 10){
+    $commentaire="je peut mieux faire!!!";
+}else {
+    $commentaire="c\'est à re-travailler";
+};
 
-echo 'Bonjour je m\'apelle'. " " .$eleve['nom'] .' '.$eleve['prenom'] ." " . 'j\'ai'." ". $eleve['age']." ".'ans et j\'ai'." ".$total." ".'de moyene';
+echo 'Bonjour je m\'apelle'. " " .$eleve['nom'] .' '.$eleve['prenom'] ." " . 'j\'ai'." ". $eleve['age']." ".'ans et j\'ai'." ".$total." ".'de moyene et'." " .$commentaire;
 
 ?>
