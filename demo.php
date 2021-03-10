805eb54f6bd8811c46f65381cddd0a04f6099387
ecoffet simon <simon.ecoffet@gmail.com>
2021-03-10 21:41:40 +0100

    php function

---
+++ demo.php
@@ -0,0 +1,27 @@
+<?php
+
+$eleve = [
+
+    'prenom' => 'Simon',
+    'nom' => 'Ecoffet',
+    'age' => 30 ,
+    'note' => [15, 20, 16]
+];
+
+$long=count($eleve['note']);
+$total =0;
+function moyene(){
+    global $eleve;
+    global $long;
+    global $total;
+    for($i = 0; $i < $long ; $i++){
+      
+        $total=$total+$eleve['note'][$i];
+    }
+    $total=$total/$long;
+};
+moyene($eleve['note']);
+
+
+echo 'Bonjour je m\'apelle'. " " .$eleve['nom'] .' '.$eleve['prenom'] ." " . 'j\'ai'." ". $eleve['age']." ".'ans et j\'ai'." ".$total." ".'de moyene';
+
